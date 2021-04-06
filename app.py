1dbcfe197e7f6fd9cb125c2902e672df49372a02
Micah <micalobia2@gmail.com>
2021-04-06 15:00:43 -0700

    Added tool endpoints

--- app.py
+++ app.py
@@ -1560,9 +1560,6 @@ def get_nh_clothing_all():
 def get_nh_photo(photo):
     authorize(DB_KEYS, request)
 
-    if not minimum_accepted_version(1,4,0):
-        abort(404, description=error_response('Resource not found.', 'Please ensure requested resource exists.'))
-
     photo = photo.replace('_',' ')
     photo_limit = '1'
     photo_tables = 'nh_photo'
@@ -1600,6 +1597,53 @@ def get_nh_photo_all():
     photo_list = get_photo_list(photo_limit, photo_tables, photo_fields)
     variation_list = get_variation_list(variation_limit, variation_tables, variation_fields)
     stitched = stitch_variation_list(photo_list, variation_list)
+
+    if request.args.get('excludedetails') == 'true':
+        return jsonify([_['en_name'] for _ in stitched])
+    else:
+        return jsonify(stitched)
+
+@app.route('/nh/tool/<string:tool>',methods=['GET'])
+def get_nh_tool(tool):
+    authorize(DB_KEYS, request)
+
+    tool = tool.replace('_',' ')
+    tool_limit = '1'
+    tool_tables = 'nh_tool'
+    tool_fields = 'identifier,en_name,uses,hha_base,buy1_price,buy1_currency,buy2_price,buy2_currency,sell,availability1,availability1_note,availability2,availability2_note,availability3,availability3_note,customizable,custom_kits,custom_body_part,version_added,unlocked,notes'
+    tool_where = f'en_name = "{tool}"'
+    tool_params = { 'action': 'cargoquery', 'format': 'json', 'tables': tool_tables, 'fields': tool_fields, 'where': tool_where, 'limit': tool_limit }
+    variation_limit = '10'
+    variation_tables = 'nh_tool_variation'
+    variation_fields = 'identifier,variation,image_url,color1,color2'
+    variation_where = f'en_name = "{tool}"'
+    variation_params = { 'action': 'cargoquery', 'format': 'json', 'tables': variation_tables, 'fields': variation_fields, 'where': variation_where, 'limit': variation_limit }
+
+    cargo_results = call_cargo(tool_params, request.args)
+    if len(cargo_results) == 0:
+        abort(404, description=error_response("No data was found for the given query.", f"MediaWiki Cargo request succeeded by nothing was returned for the parameters: {tool_params}"))
+    else:
+        piece = format_tool(cargo_results[0])
+        variations = call_cargo(variation_params, request.args)
+        return jsonify(stitch_variation(piece, variations))
+
+@app.route('/nh/tool',methods=['GET'])
+def get_nh_tool_all():
+    authorize(DB_KEYS, request)
+
+    if 'thumbsize' in request.args:
+        abort(400, description=error_response('Invalid arguments','Cannot have thumbsize in a group item request'))
+
+    tool_limit = '100'
+    tool_tables = 'nh_tool'
+    tool_fields = 'identifier,en_name,uses,hha_base,buy1_price,buy1_currency,buy2_price,buy2_currency,sell,availability1,availability1_note,availability2,availability2_note,availability3,availability3_note,customizable,custom_kits,custom_body_part,version_added,unlocked,notes'
+    variation_limit = '300'
+    variation_tables = 'nh_tool_variation'
+    variation_fields = 'identifier,variation,image_url,color1,color2'
+
+    tool_list = get_tool_list(tool_limit, tool_tables, tool_fields)
+    variation_list = get_variation_list(variation_limit, variation_tables, variation_fields)
+    stitched = stitch_variation_list(tool_list, variation_list)
 
     if request.args.get('excludedetails') == 'true':
         return jsonify([_['en_name'] for _ in stitched])
