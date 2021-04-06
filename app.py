5eb097e9856979eb60aff32ddc39040d28ce544e
Micah <micalobia2@gmail.com>
2021-04-06 14:58:27 -0700

    Added photo endpoints

--- app.py
+++ app.py
@@ -1556,5 +1556,55 @@ def get_nh_clothing_all():
     else:
         return jsonify(stitched)
 
+@app.route('/nh/photo/<string:photo>',methods=['GET'])
+def get_nh_photo(photo):
+    authorize(DB_KEYS, request)
+
+    if not minimum_accepted_version(1,4,0):
+        abort(404, description=error_response('Resource not found.', 'Please ensure requested resource exists.'))
+
+    photo = photo.replace('_',' ')
+    photo_limit = '1'
+    photo_tables = 'nh_photo'
+    photo_fields = 'identifier,en_name,_pageName=url,category,buy1_price,buy1_currency,buy2_price,buy2_currency,sell,availability1,availability1_note,availability2,availability2_note,customizable,custom_kits,custom_body_part,grid_size,length,width,height,interactable,version_added,unlocked'
+    photo_where = f'en_name = "{photo}"'
+    photo_params = { 'action': 'cargoquery', 'format': 'json', 'tables': photo_tables, 'fields': photo_fields, 'where': photo_where, 'limit': photo_limit }
+    variation_limit = '10'
+    variation_tables = 'nh_photo_variation'
+    variation_fields = 'identifier,variation,image_url,color1,color2'
+    variation_where = f'en_name = "{photo}"'
+    variation_params = { 'action': 'cargoquery', 'format': 'json', 'tables': variation_tables, 'fields': variation_fields, 'where': variation_where, 'limit': variation_limit }
+
+    cargo_results = call_cargo(photo_params, request.args)
+    if len(cargo_results) == 0:
+        abort(404, description=error_response("No data was found for the given query.", f"MediaWiki Cargo request succeeded by nothing was returned for the parameters: {photo_params}"))
+    else:
+        piece = format_photo(cargo_results[0])
+        variations = call_cargo(variation_params, request.args)
+        return jsonify(stitch_variation(piece, variations))
+
+@app.route('/nh/photo',methods=['GET'])
+def get_nh_photo_all():
+    authorize(DB_KEYS, request)
+
+    if 'thumbsize' in request.args:
+        abort(400, description=error_response('Invalid arguments','Cannot have thumbsize in a group item request'))
+
+    photo_limit = '900'
+    photo_tables = 'nh_photo'
+    photo_fields = 'identifier,en_name,_pageName=url,category,buy1_price,buy1_currency,buy2_price,buy2_currency,sell,availability1,availability1_note,availability2,availability2_note,customizable,custom_kits,custom_body_part,grid_size,length,width,height,interactable,version_added,unlocked'
+    variation_limit = '3600'
+    variation_tables = 'nh_photo_variation'
+    variation_fields = 'identifier,variation,image_url,color1,color2'
+
+    photo_list = get_photo_list(photo_limit, photo_tables, photo_fields)
+    variation_list = get_variation_list(variation_limit, variation_tables, variation_fields)
+    stitched = stitch_variation_list(photo_list, variation_list)
+
+    if request.args.get('excludedetails') == 'true':
+        return jsonify([_['en_name'] for _ in stitched])
+    else:
+        return jsonify(stitched)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
