f9826de5d6c05591710eb793973221cf7f1999b9
Kevin Payravi <kevinpayravi@gmail.com>
2021-08-30 14:06:41 -0500

    Increase limit for nh_furniture_variation

--- nookipedia/api/acnh/furniture.py
+++ nookipedia/api/acnh/furniture.py
@@ -72,7 +72,7 @@ def get_nh_furniture_all():
     furniture_limit = "1300"
     furniture_tables = "nh_furniture"
     furniture_fields = "identifier,_pageName=url,en_name=name,category,item_series,item_set,theme1,theme2,hha_category,tag,hha_base,lucky,lucky_season,function1,function2,buy1_price,buy1_currency,buy2_price,buy2_currency,sell,availability1,availability1_note,availability2,availability2_note,availability3,availability3_note,variation_total,pattern_total,customizable,custom_kits,custom_kit_type,custom_body_part,custom_pattern_part,grid_size,height,door_decor,version_added,unlocked,notes"  #'
-    variation_limit = "5350"
+    variation_limit = "6000"
     variation_tables = "nh_furniture_variation"
     variation_fields = "identifier,variation,pattern,image_url,color1,color2"
     variation_orderby = "variation_number,pattern_number"
