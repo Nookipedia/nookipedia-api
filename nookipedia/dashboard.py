c25a4dd051efb39ffa870753044f48556228d722
Kevin Payravi <kevinpayravi@gmail.com>
2026-06-02 15:28:29 -0400

    Fix dashboard handling of unauthed requests

--- nookipedia/dashboard.py
+++ nookipedia/dashboard.py
@@ -9,10 +9,9 @@ DASHBOARD_CONFIGS = config.get("APP", "DASHBOARD_CONFIGS")
 def configure_dashboard(app):
     def group_by_user():
         # Grab UUID from header or query param
-        if request.headers.get("X-API-KEY"):
-            request_uuid = request.headers.get("X-API-KEY")
-        elif request.args.get("api_key"):
-            request_uuid = request.args.get("api_key")
+        request_uuid = request.headers.get("X-API-KEY") or request.args.get("api_key")
+        if not request_uuid:
+            return "unauthenticated"
 
         # Check db for project details:
         row = db.query_db(
@@ -20,11 +19,14 @@ def configure_dashboard(app):
             [request_uuid],
             one=True,
         )
+        
         # If project details exist, use that as group_by; else, just use UUID
-        if row[2]:
+        if row and row[2]:
             return str(row[2] + " (" + row[0] + ")")
-        else:
+        elif row:
             return row[0]
+        else:
+            return request_uuid
 
     dashboard.config.group_by = group_by_user
     dashboard.config.init_from(file=DASHBOARD_CONFIGS)
