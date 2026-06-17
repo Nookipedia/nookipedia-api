d2b436c06f784b62ce581e89537002187c564ba1
zzrrbbit <zzrrbbit@users.noreply.github.com>
2026-06-17 21:39:32 +0000

    [Automated] Formatting Python with Black

--- nookipedia/cache.py
+++ nookipedia/cache.py
@@ -3,12 +3,12 @@ from pylibmc import Client as PylibmcClient
 
 mc_client = PylibmcClient(["127.0.0.1"])
 mc_client.behaviors = {
-    "connect_timeout": 1000, # ms
-    "send_timeout": 1000000, # μs (1s)
-    "receive_timeout": 1000000, # μs (1s)
-    "remove_failed": 1, # pull server from pool after failure
-    "retry_timeout": 30, # 30s
-    "dead_timeout": 30, # 30s
+    "connect_timeout": 1000,  # ms
+    "send_timeout": 1000000,  # μs (1s)
+    "receive_timeout": 1000000,  # μs (1s)
+    "remove_failed": 1,  # pull server from pool after failure
+    "retry_timeout": 30,  # 30s
+    "dead_timeout": 30,  # 30s
 }
 
 cache = Cache(config={"CACHE_TYPE": "memcached", "CACHE_MEMCACHED_SERVERS": mc_client})
